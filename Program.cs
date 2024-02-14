31f0b468204e4a75ccaa725779ef147e1c3c4eba
Henry <Ghreengate@gmail.com>
2024-02-13 21:05:48 -0500

    :hammer:new: New function

--- Program.cs
+++ Program.cs
@@ -2,9 +2,48 @@
 {
     class Program
     {
-        static void Main (string[] args) 
+        static void Main(string[] args)
         {
             Console.WriteLine("GIT FLOW");
+
+            // Ejemplo de uso de bubble sort
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            Console.WriteLine("Array sin ordenar:");
+            PrintArray(arr);
+
+            BubbleSort(arr);
+
+            Console.WriteLine("Array ordenado:");
+            PrintArray(arr);
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
